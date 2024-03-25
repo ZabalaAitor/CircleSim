2b8c055c7ff2ab365e4d2f5546fef4751a81f96b
ZabalaAitor <80098986+ZabalaAitor@users.noreply.github.com>
2024-03-25 10:26:48 +0100

    Update CircSim.py

--- src/CircSim.py
+++ src/CircSim.py
@@ -1,13 +1,13 @@
 import argparse
 import os
 import sys
-from simulate_eccDNA import SimulateEccDNA
+from simulate_circles import SimulateCircles
 from simulate_reads import SimulateReads
 from simulate_reads import read_bed_file
-from utils import parse_eccDNA_arguments, parse_reads_arguments
+from utils import parse_circles_arguments, parse_reads_arguments
 
 
-class CircSim:
+class CircleSim:
 
     def __init__(self):
         self.cs_version = "1.0"
@@ -18,23 +18,23 @@ class CircSim:
 
 version = {self.cs_version}
 
-CircSim
+CircleSim
 
 Commands:
 
-    eccDNA  Simulate eccDNA
-    reads   Simulate eccDNA reads
+    circles  Simulate circles
+    reads   Simulate circle reads
 '''
         )
         subparsers = self.parser.add_subparsers(dest='subcommand')
 
-        self.eccDNA = subparsers.add_parser(
-            name='eccDNA',
-            description='Simulate eccDNA',
-            prog='CircSim eccDNA',
-            usage='''CircSim eccDNA [options]'''
+        self.circles = subparsers.add_parser(
+            name='circles',
+            description='Simulate circles',
+            prog='CircSim circles',
+            usage='''CircSim circles [options]'''
         )
-        self.eccDNA.add_argument('-t', '--total_eccDNA', type=int, default=100, help='Total number of eccDNA to simulate')
+        self.eccDNA.add_argument('-t', '--total', type=int, default=100, help='Total number of circles to simulate')
         self.eccDNA.add_argument('-l', '--length_min', type=int, default=300, help='Minimum length of eccDNA')
         self.eccDNA.add_argument('-L', '--length_max', type=int, default=2000, help='Maximum length of eccDNA')
         self.eccDNA.add_argument('-g', '--genome_fasta', type=str, required=True, help='Path to the genome FASTA file')
@@ -42,7 +42,7 @@ Commands:
 
         self.reads = subparsers.add_parser(
             name='reads',
-            description='Simulate eccDNA reads',
+            description='Simulate circle reads',
             prog='CircSim reads',
             usage='''CircSim reads [options]'''
         )
@@ -54,7 +54,7 @@ Commands:
         self.reads.add_argument('-b', '--input_bed', type=str, required=True, help='Path to the input BED file for eccDNA')
         self.reads.add_argument('-o', '--output_fastq', type=str, default=os.getcwd(), help='Path to the output FASTQ file for reads')
 
-    def args_eccDNA(self):
+    def args_circles(self):
         return self.parser.parse_args()
 
     def args_reads(self):
@@ -63,9 +63,9 @@ Commands:
     def main(self):
         args = self.parser.parse_args()
 
-        if args.subcommand == 'eccDNA':
-            eccdna_instance = SimulateEccDNA(self.args_eccDNA())
-            eccdna_instance.run()
+        if args.subcommand == 'circles':
+            circles_instance = SimulateCircles(self.args_eccDNA())
+            circles_instance.run()
         elif args.subcommand == 'reads':
             circle_bed = read_bed_file(args.input_bed)
             reads_instance = SimulateReads(args, circle_bed)
